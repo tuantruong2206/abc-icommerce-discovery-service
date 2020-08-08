4524d03f34d432faf1c87fc2accd3be4ab52401f
tuan.truong <tuantruong2206@gmail.com>
2020-08-08 15:20:26 +0700

    initilize discovery service

---
+++ src/test/java/au/com/nab/discoveryservice/NabDiscoveryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package au.com.nab.discoveryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NabDiscoveryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
